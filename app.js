3134a045f29f95c32e663d5ec4ffabf8d1d3e0ce
Bartosz Dubinski <bartas.de@gmail.com>
2023-03-27 17:14:33 +0200

    fixed auth route in app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use("/", require("./routes/product.routes"));
 
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
